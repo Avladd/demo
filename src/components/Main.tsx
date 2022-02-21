b042b08ef8d0d6bf7a2751ea0b5f3c5663a6ba4f
Alex Vlad <avlad.dev@local.com>
2022-02-21 17:10:52 +0200

    add correct missions query

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,21 @@
 import { useQuery } from "@apollo/client";
+import { Container } from "@mui/material";
 import { MissionsDocument } from "../generated/generated";
 
 export default function Main() {
-  const { error, loading, data } = useQuery(MissionsDocument);
+  const { error, loading, data } = useQuery(MissionsDocument, {
+    variables: {
+      limit: 20,
+      order: "DESC",
+      sort: "launch_date_utc",
+    },
+  });
 
   if (error) return <pre>Error: {error.message}</pre>;
   if (loading) return <pre>Loading...</pre>;
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return (
+    <Container maxWidth="lg">
+      <pre>{JSON.stringify(data, null, 2)}</pre>;
+    </Container>
+  );
 }
